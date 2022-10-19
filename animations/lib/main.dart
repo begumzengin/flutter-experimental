4c082008252825b4d1e7d450b5392a01c46f195f
Begüm Zengin <46397735+begumzengin@users.noreply.github.com>
2022-10-19 22:32:10 +0300

    Arrange dart files

--- animations/lib/main.dart
+++ animations/lib/main.dart
@@ -1,68 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:animations/page_route_builder.dart';
 
 void main() {
-  runApp(
-    const MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Page1(),
-    ),
-  );
-}
-
-class Page1 extends StatelessWidget {
-  const Page1({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-          child: ElevatedButton(
-        onPressed: () {
-          Navigator.of(context).push(_createRoute());
-        },
-        child: const Text('GO!'),
-      )),
-    );
-  }
-}
-
-Route _createRoute() {
-  return PageRouteBuilder(
-      pageBuilder: (context, animation, secondaryAnimation) => const Page2(),
-      transitionsBuilder: ((context, animation, secondaryAnimation, child) {
-        const begin = Offset(0.0, 1.0);
-        const end = Offset.zero;
-        const curve = Curves.ease;
-        /*
-        var tween =
-            Tween(begin: begin, end: end).chain(CurveTween(curve: curve));
-
-        return SlideTransition(
-          position: animation.drive(tween),
-          child: child,
-        );
-        */
-        final tween = Tween(begin: begin, end: end);
-        final curvedAnimation =
-            CurvedAnimation(parent: animation, curve: curve);
-
-        return SlideTransition(
-          position: tween.animate(curvedAnimation),
-          child: child,
-        );
-      }));
-}
-
-class Page2 extends StatelessWidget {
-  const Page2({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: const Center(
-        child: Text('page 2'),
-      ),
     );
   }
 }
