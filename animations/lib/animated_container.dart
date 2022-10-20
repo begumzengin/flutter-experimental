0b2802fa7eeeb30b66915667cd3236b261630437
Begüm Zengin <46397735+begumzengin@users.noreply.github.com>
2022-10-20 16:48:49 +0300

    Create animated_container.dart

---
+++ animations/lib/animated_container.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class AnimatedContainerApp extends StatefulWidget {
+  const AnimatedContainerApp({super.key});
+
+  @override
+  State<AnimatedContainerApp> createState() => _AnimatedContainerAppState();
+}
+
+class _AnimatedContainerAppState extends State<AnimatedContainerApp> {
+  double _width = 50;
+  double _height = 50;
+  Color _color = Colors.green;
+  BorderRadiusGeometry _borderRadius = BorderRadius.circular(8);
+
+  @override
+  Widget build(BuildContext context) {}
+}
