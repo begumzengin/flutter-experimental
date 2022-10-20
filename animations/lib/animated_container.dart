5762b7d5eac9c85a65e8fbd31e2493ff10124c86
Begüm Zengin <46397735+begumzengin@users.noreply.github.com>
2022-10-20 17:01:22 +0300

    Rebuild the animated container with new properties

--- animations/lib/animated_container.dart
+++ animations/lib/animated_container.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 class AnimatedContainerApp extends StatefulWidget {
   const AnimatedContainerApp({super.key});
@@ -14,5 +15,46 @@ class _AnimatedContainerAppState extends State<AnimatedContainerApp> {
   BorderRadiusGeometry _borderRadius = BorderRadius.circular(8);
 
   @override
-  Widget build(BuildContext context) {}
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: const Text('animated container :)'),
+      ),
+      body: Center(
+        child: AnimatedContainer(
+          width: _width,
+          height: _height,
+          decoration: BoxDecoration(
+            color: _color,
+            borderRadius: _borderRadius,
+          ),
+          //define how long the animation should take
+          duration: const Duration(seconds: 1),
+          //provide an optional curve to make the animation feel more smoother
+          curve: Curves.fastOutSlowIn,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              final random = Random();
+
+              _width = random.nextInt(300).toDouble();
+              _height = random.nextInt(300).toDouble();
+
+              _color = Color.fromRGBO(
+                random.nextInt(256),
+                random.nextInt(256),
+                random.nextInt(256),
+                1,
+              );
+
+              _borderRadius =
+                  BorderRadius.circular(random.nextInt(100).toDouble());
+            });
+          },
+          child: const Icon(Icons.play_arrow)),
+    ));
+  }
 }
