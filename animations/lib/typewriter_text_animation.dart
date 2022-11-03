c4369846b41bea596f2e20d3acb420818873742b
Begüm Zengin <46397735+begumzengin@users.noreply.github.com>
2022-11-03 12:15:46 +0300

    Updated typer_text and created typewriter_text_aniamtion.dart

--- animations/lib/typewriter_text_animation.dart
+++ animations/lib/typewriter_text_animation.dart
@@ -2,8 +2,8 @@ import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class TyperText extends StatelessWidget {
-  const TyperText({super.key});
+class Typewriter extends StatelessWidget {
+  const Typewriter({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -12,13 +12,13 @@ class TyperText extends StatelessWidget {
           title: Text('typer',
               style: GoogleFonts.nunito(color: Color(0xFFE8D4CC))),
           backgroundColor: Color(0xFFD55C5A)),
-      body: const TyperTextPage(),
+      body: const TypewriterPage(),
     );
   }
 }
 
-class TyperTextPage extends StatelessWidget {
-  const TyperTextPage({super.key});
+class TypewriterPage extends StatelessWidget {
+  const TypewriterPage({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -27,13 +27,13 @@ class TyperTextPage extends StatelessWidget {
             child: SizedBox(
                 width: 250.0,
                 child: DefaultTextStyle(
-                  style: GoogleFonts.lobster(
+                  style: GoogleFonts.robotoSlab(
                     color: Color(0xFF001B48),
-                    fontSize: 25.0,
+                    fontSize: 30.0,
                   ),
                   child: AnimatedTextKit(animatedTexts: [
                     TyperAnimatedText(
-                        '"life\'s but a walking shadow, a poor player,\nthat struts and frets his hour upon the stage\nand then is heard no more. it is a tale\ntold by an idiot, full of sound and fury,\nsignifying nothing."\n-william shakespeare, macbeth'),
+                        'blessed are you whose worthiness gives scope\nbeing had, to triumph; being lacked, to hope.'),
                   ]),
                 ))));
   }
