2c70f9b62e515a1e0f205f7f035758623fae0f36
Begüm Zengin <46397735+begumzengin@users.noreply.github.com>
2022-11-04 16:35:09 +0300

    Create scale_text_animation.dart

---
+++ animations/lib/scale_text_animation.dart
@@ -0,0 +1,38 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class ScaleText extends StatelessWidget {
+  const ScaleText({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: Text('scale',
+              style: GoogleFonts.nunito(color: Color(0xFFE8D4CC))),
+          backgroundColor: Color(0xFFD55C5A)),
+      body: const ScaleTextPage(),
+    );
+  }
+}
+
+class ScaleTextPage extends StatelessWidget {
+  const ScaleTextPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+            child: SizedBox(
+                width: 250.0,
+                child: DefaultTextStyle(
+                    style: const TextStyle(
+                        fontSize: 70.0, fontFamily: 'Canterbury'),
+                    child: AnimatedTextKit(animatedTexts: [
+                      ScaleAnimatedText('think'),
+                      ScaleAnimatedText('build'),
+                      ScaleAnimatedText('ship'),
+                    ])))));
+  }
+}
